677405989bf4777d13af29a43824c1cd33761424
Wallace <wallace.daniel3@me.com>
2014-03-01 18:12:52 -0500

    Added index.html

--- wsgi/app/views.py
+++ wsgi/app/views.py
@@ -5,7 +5,7 @@ from flask import render_template,request,redirect,url_for,flash
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html'
+	return render_template('index.html')
 
 @app.route('/testing')
 def testing():
