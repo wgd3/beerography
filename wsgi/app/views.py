42ae41a152a34136a0f13791c92dcd5ef7056e79
Wallace <wallace.daniel3@me.com>
2014-03-02 21:42:03 -0500

    untapped base.gtmk

--- wsgi/app/views.py
+++ wsgi/app/views.py
@@ -1,24 +1,35 @@
 from app import app
 import json
-from flask import render_template,redirect,url_for,flash,request
-import urllib2
+from flask import render_template,redirect,url_for,flash,request,session
+import urllib2,urllib
 
 MY_CLIENT_ID = "C361DB96FBB835383819048F80A189EC40DE1FB2"
 MY_CLIENT_SECRET = "9A3A9169B14B23A48C7EECEF2E29692986671872"
 ACC_TOKEN = ''
 
+app.secret_key = 'wallace'
+
 @app.route('/', methods=['GET','POST'])
-@app.route('/index', methods=['GET','POST'])
+@app.route('/index')
 def index():
-	if request.method == 'POST':
-		beerQuery = request.form['beer-search']
-		if (beerQuery != ''):
-			beerResults = BreweryDb.search({'type':'beer','q':beerQuery})
-			#flash('Query on successful')
-			return render_template('index.html', results=beerResults, query=beerQuery)
-		else:
-			flash('There was a problem with your search')
+
+	# check to see if the session with valid key has been set
+	if token in session:
+		user = []
+		# create query for bais cuser info 
+		args = {'access_token':session['token'],'compact':'true'}
+		data = urllib.urlencode(args)
+		url = 'http://api.untappd.com/v4/user/info?'+data
+		request = urllib2.Request(url)
+		response = urllib2.urlopen(request)
+		user_json = json.loads(response.read())
+
+		# I think we can just send the json data to the jinja2 template for parsing
+		print "Token found in session, sending user json data to page"
+		return render_template('index.html',user=user_json)
 	
+	# session must not be set
+	print "No token found in session, return vanilla template"
 	return render_template('index.html')
 
 @app.route('/testing')
@@ -44,7 +55,7 @@ def redirect():
 			print "Found tempCode in request: " + tempCode
 			print "Getting token code..."
 			# create Request object with custom URL
-			token_req = urllib2.Request("https://untappd.com/oauth/authorize/?client_id="+MY_CLIENT_ID+"&client_secret="+MY_CLIENT_SECRET+"&response_type=code&redirect_url=http://localhost:5000/redirect&code="+tempCode)
+			token_req = urllib2.Request("https://untappd.com/oauth/authorize/?client_id="+MY_CLIENT_ID+"&client_secret="+MY_CLIENT_SECRET+"&response_type=code&redirect_url=http://192.168.2.8:5000/redirect&code="+tempCode)
 			
 			# open the custom URL and store the result
 			token_resp = urllib2.urlopen(token_req)
@@ -56,15 +67,38 @@ def redirect():
 			print data['response']
 			ACC_TOKEN = data['response']['access_token']
 			print "Token code found: " + ACC_TOKEN	
-		
+			session['token'] = ACC_TOKEN	
+			flash('Logged in successfully')
+			loginSuccess = True
 		else:
 			flash("Error getting tempCode for access token")
 			print "Error while obtaining tempCode"
+			loginSuccess = False
 	else:
 		print "Non-GET method for /redirect"
 		flash("Error with redirect")
 
-	return render_template('testing.html')
+	return render_template(url_for('index'))
 
+@app.route('/testuser')
+def loggedin():
 
+	if 'token' in session:
+		# Set obvious logged in variable because a session token was found
+		loggedIn = True
+
+		# Create URL args for user info
+		user_args = {'access_token':session['token'],'compact':'true'}
+		data = urllib.urlencode(user_args)
+		print "Encoded: "+str(data)
+		url = 'http://api.untappd.com/v4/user/info?'+data
+		request = urllib2.Request(url)
+		response = urllib2.urlopen(request)
+		user_info = json.loads(response.read())
+
+		# for debugging, print some shit
+		print "Found some data: "+str(user_info['response']['user'])
+	else:
+		loggedIn = False
+	return render_template('testuser.html',loggedIn=loggedIn)
 
