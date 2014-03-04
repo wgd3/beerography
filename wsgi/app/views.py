a5f81d9a446d4be6a4fe3c8cfaaa47adf4c43f6d
Wallace Daniel <wdaniel@redhat.com>
2014-03-04 16:04:46 -0500

    Major changes! Integrated Untappd (and auth), added footer, updated navbar links

--- wsgi/app/views.py
+++ wsgi/app/views.py
@@ -1,10 +1,11 @@
 from app import app
 import json
 from flask import render_template,redirect,url_for,flash,request
-from brewerydb import *
+import urllib2
 
-# Set up brewerydb connection
-BreweryDb.configure("83d7702a58244b96d38c625c9a474355")
+MY_CLIENT_ID = "C361DB96FBB835383819048F80A189EC40DE1FB2"
+MY_CLIENT_SECRET = "9A3A9169B14B23A48C7EECEF2E29692986671872"
+ACC_TOKEN = ''
 
 @app.route('/', methods=['GET','POST'])
 @app.route('/index', methods=['GET','POST'])
@@ -30,12 +31,40 @@ def testing():
 	units = 'mi'
 	geo = 1
 
-	# get sample results
-	localBrews = BreweryDb.search({'type':'beer','q':'hoppyum'})
-	print "Found this many breweries:"+str(localBrews)
 
-	# beer search testing
-	#foundBeers = BreweryDb.search({'type': 'beer','q':'Lonerider'})
-	#print "Found some beer: "+str(foundBeers)
+	return render_template('testing.html',client_id=MY_CLIENT_ID)
 
-	return render_template('testing.html',results=localBrews)
+@app.route('/redirect', methods=['GET'])
+def redirect():
+	if request.method == 'GET':
+		print "Detected GET request on /redirect"
+		tempCode = request.args.get("code")
+		if tempCode != '':
+			
+			print "Found tempCode in request: " + tempCode
+			print "Getting token code..."
+			# create Request object with custom URL
+			token_req = urllib2.Request("https://untappd.com/oauth/authorize/?client_id="+MY_CLIENT_ID+"&client_secret="+MY_CLIENT_SECRET+"&response_type=code&redirect_url=http://localhost:5000/redirect&code="+tempCode)
+			
+			# open the custom URL and store the result
+			token_resp = urllib2.urlopen(token_req)
+			
+			# grab the data from the response and parse it in JSON format
+			data = json.loads(token_resp.read())
+			
+			# update the ACC_TOKEN variable
+			print data['response']
+			ACC_TOKEN = data['response']['access_token']
+			print "Token code found: " + ACC_TOKEN	
+		
+		else:
+			flash("Error getting tempCode for access token")
+			print "Error while obtaining tempCode"
+	else:
+		print "Non-GET method for /redirect"
+		flash("Error with redirect")
+
+	return render_template('testing.html')
+
+
+
