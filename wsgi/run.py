3b583436aa4bcddb232becf26ba0cc0d6f0414f9
Wallace Daniel <wdaniel@redhat.com>
2014-03-08 13:15:25 -0500

    can't get this to run on openshift instance

--- wsgi/run.py
+++ wsgi/run.py
@@ -2,4 +2,4 @@ from app import app
 from flask import Flask
 
 if __name__ == "__main__":
-    app.run(debug = True,host='0.0.0.0') #We will set debug false in production 
+    app.run(debug = True) #We will set debug false in production 
